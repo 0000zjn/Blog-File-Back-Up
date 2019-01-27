c9ea9a232a47c33cd281272e70a5b80fa06d0d99
ZhaoJianning <269468600@qq.com>
2019-01-27 11:33:11 +0800

    add photos

--- tool.py
+++ tool.py
@@ -156,7 +156,7 @@ def cut_photo():
         else:
             pass
     else:
-        print("source directory not exist!")     
+        print("source directory not exist!")  
 
 
 
@@ -172,7 +172,7 @@ def git_operation():
     os.system('git push origin master')
 
 if __name__ == "__main__":
-    cut_photo()        # 裁剪图片，裁剪成正方形，去中间部分，保存到temp文件夹
+    cut_photo()        # 裁剪图片，裁剪成正方形，去中间部分
     compress_photo()   # 压缩图片，并保存到mini_photos文件夹下
     git_operation()    # 提交到github仓库
     handle_photo()     # 将文件处理成json格式，存到博客仓库中
